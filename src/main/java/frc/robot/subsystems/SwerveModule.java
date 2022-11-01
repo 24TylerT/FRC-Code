1c1df2765e380514b31a04f53f5e815c2b213ff8
24TylerT <88692512+24TylerT@users.noreply.github.com>
2022-11-01 16:31:13 +0800

    fixed mistakes, i might have not import some stuff

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -7,20 +7,42 @@ import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import com.ctre.phoenix.CANCoder;
 
+
 public class SwerveModule {
-    TalonSRX mRotor, mThrottle;
+    //useful variables
+    WPI_TalonFX mRotor, mThrottle;
     CANCoder mRotorEncoder;
 
-    public SwerveModule(int rotorID, int throttleID, int CANCoderID, boolean throttleReversed){
-        mRotor = new TalonSRX(rotorID);
-        mThrottle = new TalonSRX(throttleID);
+    //swervemodule constructor
+    public SwerveModule(int rotorID, int throttleID, int CANCoderID, boolean throttleReversed, double rotorOffsetAngle){
+        mRotor = new WPI_TalonFX(rotorID);
+        mThrottle = new WPI_TalonFX(throttleID);
 
         mRotorEncoder = new CANCoder(CANCoderID);
 
+        mRotor.configFactoryDefault();
+        mThrottle.configFactoryDefault();
+        mRotorEncoder.configFactoryDefault();
+
+        mRotorEncoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);
+        mRotorEncoder.configMagnetOffset(rotorOffsetAngle);
+        mRotorEncoder.configSensorDirection(false);
+        mRotorEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+
+        mRotor.setInverted(TalonFXInvertType.Clockwise);
         mThrottle.setInverted(throttleReversed);
+
+        mRotor.enableVoltageCompSaturation(true);
+        mThrottle.enableVoltageCompSaturation(true);
+        mRotor.configVoltageCompSaturation(Constants.kVoltageCompensation);
+        mThrottle.configVoltageCompSaturation(Constants.kVoltageCompensation);
+
+        mRotor.setNeutralMode(NeutralMode.Brake);
+        mThrottle.setNeutralMode(NeutralMode.Brake);
     }
 
 
